c1b51ae23de302aa75bcc94820f526e733aadae5
moshthepitt <kelvin@jayanoris.com>
2015-11-17 15:24:55 +0300

    add a logout method and views

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,7 @@
 import json
 
 from django.utils.translation import ugettext_lazy as _
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 from rest_framework import permissions, viewsets, status, views
 from rest_framework.response import Response
@@ -65,3 +65,12 @@ class LoginView(views.APIView):
                 'status': _('Unauthorized'),
                 'message': _('Username/password combination invalid.')
             }, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class LogoutView(views.APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, format=None):
+        logout(request)
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -5,7 +5,7 @@ from rest_framework_nested import routers
 
 from thinkster_django_angular_boilerplate.views import IndexView
 
-from authentication.views import AccountViewSet, LoginView
+from authentication.views import AccountViewSet, LoginView, LogoutView
 
 router = routers.SimpleRouter()
 router.register(r'accounts', AccountViewSet)
@@ -13,6 +13,7 @@ router.register(r'accounts', AccountViewSet)
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
+    url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
     url(r'^api/v1/', include(router.urls)),
     url(r'^.*$', IndexView.as_view(), name='index'),
 ]
