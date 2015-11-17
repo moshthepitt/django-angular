741faf706ecdb3c7b34c42041077c091b14092a8
moshthepitt <kelvin@jayanoris.com>
2015-11-17 13:45:28 +0300

    set up Django rest framework with Account app

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,9 +1,17 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from rest_framework_nested import routers
+
 from thinkster_django_angular_boilerplate.views import IndexView
+
+from authentication.views import AccountViewSet
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/v1/', include(router.urls)),
     url(r'^.*$', IndexView.as_view(), name='index'),
 ]
